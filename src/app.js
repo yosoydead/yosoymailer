c0060498e6acd185530abaae166f0b84a5641c32
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-08-11 01:58:03 +0300

    testare timp cronjob

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  console.log('home route');
-  mailer();
+  // console.log('home route');
+  // mailer();
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
   });
@@ -30,6 +30,8 @@ app.use(middlewares.errorHandler);
 // eslint-disable-next-line max-len
 // În fiecare vineri la prânz. Așa mă asigur și că aplicația merge și nu dă rateuri ca să aflu abia după 1 lună.
 // Rămâne să mă gândesc dacă ăsta o să fie intervalul final. Momentan, așa rămâne.
-schedule.scheduleJob('0 14 * * FRI', mailer);
+// schedule.scheduleJob('0 14 * * FRI', mailer);
+
+schedule.scheduleJob('*/1 * * * *', mailer);
 
 module.exports = app;
